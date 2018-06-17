f9a832f2d3653df918687b44e96417bf7eaf491c
xxlv <lvxx@yidianling.com>
2018-06-17 18:10:03 +0800

    Fix typo

--- quick_generator/template/${PLACE}.java
+++
@@ -1,6 +0,0 @@
-package com.${PACKAGE_LEVEL1}.${PACKAGE_LEVEL2}.intf.po;
-
-
-public class ${PLACE} {
-
-}
--- quick_generator/template/${PLACE}Mapper.java
+++
@@ -1,7 +0,0 @@
-package com.${PACKAGE_LEVEL1}.${PACKAGE_LEVEL2}.service.dao;
-
-import com.${PACKAGE_LEVEL1}.common.service.BaseMapper;
-import com.${PACKAGE_LEVEL1}.${PACKAGE_LEVEL2}.intf.po.${PLACE};
-
-public interface ${PLACE}Mapper extends BaseMapper<${PLACE}> {
-}
